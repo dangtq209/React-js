a9cd4dc2b735b9b3151d11aa91c8d5efb3994e52
Tran Dang <dangtq209@gmail.com>
2020-03-04 14:27:26 +0700

    GET Data API

--- dangtq/src/App.js
+++ dangtq/src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Detail from './components/Detail'
-import Page404 from './components/Page404'
-import Footer from './components/Footer'
+import Table from './components/Table'
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Header />
-        <div id="body">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/detail" component={Detail} />
-            <Route component={Page404} />
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
+      <div id="wrapper" className="container">
+        <h2>Quản lý thành viên</h2>
+        <p>Demo đơn giản về hiển thị danh sách thành viên thông qua việc <b>GET Data API bằng Axios Module</b></p>
+        <Table />
+      </div>
     )
   }
 }
